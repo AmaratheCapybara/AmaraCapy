e26c6515d585d8bcc232a027cff91d4d9d1e9731
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-02-12 14:34:12 -0500

    making it work
    i tried -A

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,10 +2,12 @@
 </script>
 
 <nav>
+
 	<ul>
 		<li>Subscription Box</li>
-		<li><a href="../../routes/Products">Products</a></li>
-		<li><a href="../../routes/Location">Location</a></li>
-		<li><a href="../../routes/PaymentOptions">Payment Options</a></li>
+		<li><a class="nava" href="../../routes/Products">Products</a></li>
+		<li><a class="nava" href="../../routes/Location">Location</a></li>
+		<li><a class="nava" href="../../routes/PaymentOptions">Payment Options</a></li>
 	</ul>
+
 </nav>
