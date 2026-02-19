878ee5619e2d1dacb4004129c609509082c4f807
AmaratheCapybara <AmaranthAragon@gmail.com>
2026-02-19 16:20:56 -0500

    Create Navbar.svelte

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<nav>
+<div></div>
+	<ul>
+		<li><a class="nava" href="../../routes/MonthlyBoxes">Subscription Box</a></li>
+		<li><a class="nava" href="../../routes/Products">Products</a></li>
+		<li><a class="nava" href="../../routes/Location">Location</a></li>
+		<li><a class="nava" href="../../routes/PaymentOptions">Payment Options</a></li>
+	</ul>
+
+</nav>
